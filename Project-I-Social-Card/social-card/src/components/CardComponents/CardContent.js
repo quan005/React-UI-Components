1703a6531cb29db82ff5e08273b2222c38e3322e
quan005 <ralphealfreeman@gmail.com>
2019-05-08 11:25:23 -0700

    social card done

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = props => {
+  return (
+    <div className="card-content-container">
+      <h5>{props.title || "Get started with React"}</h5>
+      <p>{props.paragraph || "React makes it painless to create interactive UI's. Design simple views for each state in your application."}</p>
+      <span>{props.url || "reactjs.org"}</span>
+    </div>
+  );
+};
+
+export default CardContent;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = props => {
+  return (
+    <div className="title-container">
+      <h4>{props.title || "Lambda School"}</h4>
+      <span>{props.subtitle || "@Lambda School"}</span>
+      <span>{props.date || "26 Jan"}</span>
+    </div>
+  );
+};
+
+export default HeaderTitle;
